1f55b5490de10eec1ff0cfae32fe8760f7d55ae8
Daniel Goldstein <dpgold94@gmail.com>
2014-04-02 22:00:14 -0500

    Added a base for the sliding menu

--- GrinnellPlans/src/edu/grinnell/grinnellplans/MainActivity.java
+++ GrinnellPlans/src/edu/grinnell/grinnellplans/MainActivity.java
@@ -1,22 +1,30 @@
 package edu.grinnell.grinnellplans;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+public class MainActivity extends SherlockActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+	SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setMenu(R.layout.sliding_menu);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//	// Inflate the menu; this adds items to the action bar if it is present.
+//	getMenuInflater().inflate(R.menu.main, menu);
+//	return true;
+//    }
 
 }
